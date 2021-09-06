44316af5596d8d420cdf3cc1945e457482311edb
wil2495 <alexisfranco2495@hotmail.com>
2021-09-05 23:19:29 -0500

    Inicializar Sdk de Firebase.

--- sign_up/lib/main.dart
+++ sign_up/lib/main.dart
@@ -1,1 +1,37 @@
-void main() {}
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Sign_up Google",
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text("Bienvenido"),
+        ),
+      ),
+    );
+  }
+}
