4da66098ae68f6d5ee8c4830281890458d474c40
wil2495 <alexisfranco2495@hotmail.com>
2021-09-06 11:51:20 -0500

    Creación de input_field -globalsWidgets.

--- sign_up/lib/app/ui/pages/register/register_page.dart
+++ sign_up/lib/app/ui/pages/register/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sign_up/app/ui/global_widgets/custom_input_field.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -16,12 +17,55 @@ class RegisterPage extends StatelessWidget {
           padding: const EdgeInsets.all(15.0),
           child: Column(
             children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: "name"),
+              CustomInputField(
+                label: "Name",
+                onChanged: (text) {},
+                validator: (text) {
+                  if (text == null) return null;
+                  //   return isValidName(text) ? null : "invalid name";
+                },
               ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "last name"),
-              )
+              const SizedBox(
+                height: 15,
+              ),
+              CustomInputField(
+                  label: "Last Name",
+                  onChanged: (text) {},
+                  validator: (text) {
+                    if (text == null) return null;
+                    // return isValidName(text) ? null : "invalid last name";
+                  }),
+              const SizedBox(height: 15),
+              CustomInputField(
+                  label: "Email",
+                  inputype: TextInputType.emailAddress,
+                  onChanged: (text) {},
+                  validator: (text) {
+                    print("text $text");
+                    if (text == null) return null;
+                    //  return isValidEmail(text) ? null : "invalid email";
+                  }),
+              const SizedBox(height: 15),
+              CustomInputField(
+                label: "Password",
+                isPassword: true,
+                inputype: TextInputType.visiblePassword,
+                onChanged: (text) {},
+                validator: (text) {
+                  if (text == null) return null;
+                  if (text.trim().length >= 6) {
+                    return null;
+                  } else {
+                    return "Invalid password";
+                  }
+                },
+              ),
+              const SizedBox(height: 15),
+              CustomInputField(
+                label: "Verification Password",
+                isPassword: true,
+                onChanged: (text) {},
+              ),
             ],
           ),
         ),
